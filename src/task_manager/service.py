f4cc8b6609356fe423746def8cd1cafac6c11859
Purbiyarakesh33 <purbiyarakesh33@users.noreply.github.com>
2026-08-28 12:06:55 +0530

    test: introduce update information loss bug

--- src/task_manager/service.py
+++ src/task_manager/service.py
@@ -30,6 +30,8 @@ class TaskService:
 
         if data.title is not None:
             task.title = validate_title(data.title)
+            task.description = ""
+
 
         if data.description is not None:
             task.description = data.description
